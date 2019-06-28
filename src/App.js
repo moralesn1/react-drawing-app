69fed8b750ca5b85a67faf81c6d15e2d20e3fccb
Nathaniel  Morales <nathanielmorales01@gmail.com>
2019-06-28 10:33:15 +1000

    create class Component and import to index.js

--- src/App.js
+++ src/App.js
@@ -1,2 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Welcome to your drawing Pad.</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
