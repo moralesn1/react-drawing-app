ec57af27ec54386c6497979b3ff9bec65483ef36
Nathaniel  Morales <nathanielmorales01@gmail.com>
2019-06-28 12:08:03 +1000

    remove bind(this) to function using es6 fat arrow syntax to bypass this

--- src/ColourSelector.js
+++ src/ColourSelector.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 
 class ColourSelector extends Component {
-  constructor(props) {
-    super(props);
-    this.onInputChange = this.onInputChange.bind(this);
-  }
   static defaultProps = {
     hex: "#f4424b"
   };
@@ -13,9 +9,9 @@ class ColourSelector extends Component {
     hex: this.props.hex
   };
 
-  onInputChange(event) {
+  onInputChange = event => {
     console.log(this.state.hex);
-  }
+  };
 
   render() {
     const { hex } = this.state;
