1796a31d6827a3688e9ee4233c283892d96874f3
Nathaniel  Morales <nathanielmorales01@gmail.com>
2019-06-28 13:11:08 +1000

    function on event to draw on canvas

--- src/Canvas.js
+++ src/Canvas.js
@@ -7,15 +7,65 @@ const styles = {
 
 class Canvas extends Component {
   state = {
-    hex: "#f4424b"
+    hex: "#f4424b",
+    coords: null,
+    height: 400,
+    width: 400
   };
 
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef();
+    this.context = null;
+  }
+
+  componentDidMount() {
+    console.log(this.canvasRef);
+  }
+
   componentDidUpdate = () => {
-    console.log(this.state.hex);
+    this.setContext();
+  };
+
+  setContext = () => {
+    this.context = this.canvasRef.current.getContext("2d");
+    this.context.strokeStyle = this.state.hex;
+    this.context.lineJoin = "round";
+    this.context.lineWidth = 3;
   };
 
   onColourSelectorChange = hex => {
     this.setState({ hex });
+  };
+
+  onCanvasMouseDown = event => {
+    const x = event.nativeEvent.offsetX;
+    const y = event.nativeEvent.offsetY;
+    this.setState({ coords: [x, y] });
+  };
+
+  onCanvasMouseMove = event => {
+    const x = event.nativeEvent.offsetX;
+    const y = event.nativeEvent.offsetY;
+    const coords = this.state.coords;
+    const { height, width } = this.state;
+
+    if (x > 0 && x < width && y > 0 && y < height) {
+      if (coords) {
+        this.context.beginPath();
+        this.context.moveTo(coords[0], coords[1]);
+        this.context.lineTo(x, y);
+        this.context.closePath();
+        this.context.stroke();
+        this.setState({ coords: [x, y] });
+      }
+    } else {
+      this.setState({ coords: null });
+    }
+  };
+
+  onCanvasMouseUp = event => {
+    this.setState({ coords: null });
   };
 
   render = () => {
@@ -29,7 +79,15 @@ class Canvas extends Component {
           />
         </div>
         <div>
-          <canvas width="400" height="400" style={styles} />
+          <canvas
+            ref={this.canvasRef}
+            width="400"
+            height="400"
+            style={styles}
+            onMouseMove={this.onCanvasMouseMove}
+            onMouseDown={this.onCanvasMouseDown}
+            onMouseUp={this.onCanvasMouseUp}
+          />
         </div>
       </>
     );
